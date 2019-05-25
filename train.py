700b88b860d4208de2e34820db9fe191ca5dffeb
ssadik1 <sadiksatir@gmail.com>
2019-05-25 16:48:36 +0300

    train --> (eps+26000) for noise

--- train.py
+++ train.py
@@ -60,7 +60,7 @@ def train(device):
             state = env.reset(relaunch=eps%100 == 0, render=False, sampletrack=True)
             epsisode_reward = 0
             episode_value = 0
-            sigma = (hyprm.start_sigma-hyprm.end_sigma)*(max(0, 1-eps/hyprm.episodes)) + hyprm.end_sigma
+            sigma = (hyprm.start_sigma-hyprm.end_sigma)*(max(0, 1-(eps+26000)/hyprm.episodes)) + hyprm.end_sigma
             randomprocess = OrnsteinUhlenbeckProcess(hyprm.theta, sigma, outsize)
             for i in range(hyprm.maxlength):
                 torch_state = agent._totorch(state, torch.float32).view(1, -1)
